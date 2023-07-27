c1bbe178d1a1ffaeae8e26a50f3e3facf32c519b
Ezequiel Figueiredo Dawud <126517272+ezequiel-dawud@users.noreply.github.com>
2023-07-27 11:19:56 -0300

    atualizaçãoi

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 
-let ano = coument.getnewDate();
-document.write("Eu tenho" + (ano - 1987 ) + "anos");
+const anoAtual = new Date();
+let ano = anoAtual.getFullYear();
+document.write("Eu tenho " + (ano - 1987) + " anos <br>");
+let media = (34 + 66 + 33) / 3;
+document.write("Hello <br>");
+document.write(`A media das idades é ${Math.round(media)}<br>`);
